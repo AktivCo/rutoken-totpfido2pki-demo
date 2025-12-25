6a01063c3292b327416a05dff5c5fc3b11107ac8
b.vazeev <b.vazeev@aktiv-company.ru>
2025-12-25 12:04:07 +0300

    back: Upgrade .NET from version 6 to 8
    Resolve: WEB-5333

--- RutokenTotpFido2Demo/Entities/CertificateData.cs
+++ RutokenTotpFido2Demo/Entities/CertificateData.cs
@@ -3,14 +3,14 @@
     public class CertificateData
     {
         public int Id { get; set; }
-        public string SerialNumber { get; set; }
+        public required string SerialNumber { get; set; }
         public int UserId { get; set; }
-        public User User { get; set; }
+        public User? User { get; set; }
 
         /// <summary>
         /// Full bc subject public key info as base64 string
         /// </summary>
-        public string PublicKeyInfo { get; set; }
+        public required string PublicKeyInfo { get; set; }
         public DateTime? LastLoginDate { get; set; }
     }
 }
--- RutokenTotpFido2Demo/Migrations/EfDbContextModelSnapshot.cs
+++ RutokenTotpFido2Demo/Migrations/EfDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace RutokenTotpFido2Demo.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.8")
+                .HasAnnotation("ProductVersion", "8.0.22")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
